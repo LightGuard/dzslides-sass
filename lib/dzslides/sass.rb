846d65bd8365999d42b7b423833b57751f59d50e
LightGuard <lightguard.jp@gmail.com>
2013-04-01 15:57:57 -0600

    Initial project layout

---
+++ lib/dzslides/sass.rb
@@ -0,0 +1,7 @@
+require "dzslides/sass/version"
+
+module Dzslides
+  module Sass
+    # Your code goes here...
+  end
+end
---
+++ lib/dzslides/sass/version.rb
@@ -0,0 +1,5 @@
+module Dzslides
+  module Sass
+    VERSION = "0.0.1"
+  end
+end
